810d5d0b54bc4377f5f95936d8b78a99aa93bb88
Anastasiia Boiko <nastasyah86@gmail.com>
2021-07-28 20:46:50 +0300

    basic pagination done

--- src/Сomponents/ListComments/ListComments.jsx
+++ src/Сomponents/ListComments/ListComments.jsx
@@ -1,47 +1,63 @@
 import { useState, useEffect } from 'react';
 import { Button } from '@material-ui/core';
-import axios from 'axios'
+import axios from 'axios';
 
 function dateformat(date) {
-  const data = new Date(date)
-  let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  return data.toLocaleString('ru',options)
+  const data = new Date(date);
+  let options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+  return data.toLocaleString('ru', options);
 }
-
 
 export default function ListComments() {
   const [comment, setComment] = useState([]);
-  const [page, setPage] = useState(1);
-  const [loading, setLoading] = useState(false)
-  const [commentsPerPage, setCommentsPerPage]=useState(10)
+  const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [commentsPerPage] = useState(10);
 
   useEffect(() => {
-    const fetchComments = async ()=>
-    fetch(`https://jordan.ashton.fashion/api/goods/30/comments?page=${page}`)
-      .then(res => res.json())
-      .then(json => setComment(json));
-  }, [page]);
+    const fetchComments = async () => {
+      setLoading(true);
+      const res = await axios.get(
+        `https://jordan.ashton.fashion/api/goods/30/comments`
+      );
+      setComment(res.data);
+      setLoading(false);
+    };
+    fetchComments();
+  }, []);
 
-  const loadPage = () => setPage(+page + 1);
+  const indexOfLastComment = currentPage * commentsPerPage;
+  const indexOfTheFirstComment = indexOfLastComment - commentsPerPage;
+  const currentComments = comment?.data?.slice(
+    indexOfTheFirstComment,
+    indexOfLastComment
+  );
+  console.log(currentComments);
+  const loadPage = () => setCurrentPage(+currentPage + 1);
 
-  
   return (
-    <div className="CommentsWrapper">
+    <div className='CommentsWrapper'>
       <h1>Comments</h1>
 
       <ul>
-         {comment.data && comment.data.map(({ name, text, created_at, id }) => (
-          <li key={id}>
-            <h2 className="name">{name}</h2>
-            <p className="message">{text}</p>
-            <p className="data">{dateformat(created_at)}</p>
-          </li>
-        ))}
+        {!loading &&
+          currentComments.map(({ name, text, created_at, id }) => (
+            <li key={id}>
+              <h2 className='name'>{name}</h2>
+              <p className='message'>{text}</p>
+              <p className='data'>{dateformat(created_at)}</p>
+            </li>
+          ))}
       </ul>
       <Button
         onClick={loadPage}
-        variant="contained"
-        color="secondary"
+        variant='contained'
+        color='secondary'
         style={{ margin: '60px auto 10px auto', display: 'block' }}
       >
         load more
